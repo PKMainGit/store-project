b48e7523195c59268ab033fae9015deced7681bc
PKdev <152315716+PKMainGit@users.noreply.github.com>
2024-03-30 12:02:59 +0100

    clear template

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import './side-bar/sidebar';
-import './hero/hero';
-import './modal/modal';
-import './ToUpBtn';
-import './fonds/support-ukraine';
-import './dark';
-import './loader/loader';
--- src/js/modal/ls-modal.js
+++
@@ -1,7 +0,0 @@
-export function loadFromLS(key) {
-  return JSON.parse(localStorage.getItem(key)) || [];
-}
-
-export function saveToLS(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
